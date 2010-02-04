31ddbc8a88339c4da8e08ebff3565d631e3c3010
Peter Schröder <nofail@peter-schroders-macbook.local>
2010-02-04 20:56:56 +0100

    add some controller

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,11 +1,22 @@
 class BlogController < ApplicationController
-  
+
   def index
     puts "index"
   end
-  
-  def create
-    puts "create"
+
+  def new
+    puts "new"
+    @blog = Blog.new
   end
-  
+
+  def create
+    @blog = Blog.new params[:blog]
+    if @blog.valid?
+      flash[:notice] = "Successfully created post"
+      render :index
+    else
+      render :new
+    end
+  end
+
 end
